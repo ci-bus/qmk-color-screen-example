4429fb2b49ce84fec162ac733cac056a57498451
Miguel Calero <miguel.calero@bimaxpro.net>
2023-06-29 17:57:33 +0200

    fix

--- 70special/keymaps/default/keymap.c
+++ 70special/keymaps/default/keymap.c
@@ -17,7 +17,7 @@ static painter_device_t tft;
 static painter_image_handle_t car_image;
 
 void keyboard_post_init_user(void) {
-    tft = qp_st7789_make_spi_device(128, 128, TFT_CS_PIN, TFT_DC_PIN, TFT_RST_PIN, 8, 0);
+    tft = qp_st7735_make_spi_device(128, 128, TFT_CS_PIN, TFT_DC_PIN, TFT_RST_PIN, 8, 0);
     qp_init(tft, QP_ROTATION_0);
 
     car_image = qp_load_image_mem(gfx_car);
