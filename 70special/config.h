36c2228745f0cbf48e0e18fbf0e7569e1d3c17b7
Miguel Calero <miguel.calero@bimaxpro.net>
2023-06-30 11:40:08 +0200

    fix offset and rgb565 image

--- 70special/config.h
+++ 70special/config.h
@@ -3,11 +3,12 @@
 #define PICO_FLASH_SIZE_BYTES (512 * 1024)
 
 // Display Configuration
-#define ST7735_NUM_DEVICES 1
+#define SPI_SCK_PIN GP19
+#define SPI_MOSI_PIN GP18
+#define SPI_MISO_PIN GP17
 
-#define SPI_SCK_PIN GP18
-#define SPI_MOSI_PIN GP19
+#define TFT_CS_PIN GP12
+#define TFT_DC_PIN GP11
+#define TFT_RST_PIN GP10
 
-#define TFT_CS_PIN GP3
-#define TFT_DC_PIN GP4
-#define TFT_RST_PIN GP5
+#define QUANTUM_PAINTER_SUPPORTS_NATIVE_COLORS TRUE
--- 70special/keymaps/default/keymap.c
+++ 70special/keymaps/default/keymap.c
@@ -5,23 +5,20 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT_65_all(KC_A), [1] = LAYOUT_65_all(KC_B)};
 
-void keyboard_pre_init_user(void) {
-#ifdef __AVR__
-    // BIT-C turn off led
-    setPinOutput(F0);
-    writePinLow(F0);
-#endif
-}
-
 static painter_device_t tft;
 static painter_image_handle_t car_image;
 
 void keyboard_post_init_user(void) {
-    tft = qp_st7735_make_spi_device(128, 128, TFT_CS_PIN, TFT_DC_PIN, TFT_RST_PIN, 8, 0);
+    tft = qp_st7735_make_spi_device(128, 128, TFT_CS_PIN, TFT_DC_PIN, TFT_RST_PIN, 4, 0);
     qp_init(tft, QP_ROTATION_0);
+
+    qp_set_viewport_offsets(tft, 2, 1);
+
+    qp_rect(tft, 0, 0, 127, 127, 0, 0, 0, true);
+    qp_flush(tft);
 
     car_image = qp_load_image_mem(gfx_car);
     if (car_image != NULL) {
-        qp_drawimage(tft, 128, 128, car_image);
+        qp_drawimage(tft, 0, 0, car_image);
     }
 }
